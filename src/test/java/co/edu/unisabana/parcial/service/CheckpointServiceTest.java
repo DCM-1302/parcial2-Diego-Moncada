144687e6fd3563eb41b7785afd7a0ffdc154509e
dcm1302 <dcmonave@gmail.com>
2024-09-30 08:39:18 -0500

    tests service

--- src/test/java/co/edu/unisabana/parcial/service/CheckpointServiceTest.java
+++ src/test/java/co/edu/unisabana/parcial/service/CheckpointServiceTest.java
@@ -6,6 +6,7 @@ import co.edu.unisabana.parcial.service.model.Checkout;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -15,6 +16,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CheckpointServiceTest {
+
+    @Mock
+    CheckpointDAO checkpointPort;
 
     @InjectMocks
     CheckpointService checkpointService;
