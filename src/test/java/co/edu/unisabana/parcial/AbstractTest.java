2ec0e604113432de28d395abb6024f1138092ed1
dcm1302 <dcmonave@gmail.com>
2024-09-30 08:32:12 -0500

    update: Si no sirve es pq no se alcanzó a instalar el jdk 17

--- src/test/java/co/edu/unisabana/parcial/AbstractTest.java
+++ src/test/java/co/edu/unisabana/parcial/AbstractTest.java
@@ -1,6 +1,7 @@
 package co.edu.unisabana.parcial;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -8,5 +9,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class AbstractTest {
+
 
 }
